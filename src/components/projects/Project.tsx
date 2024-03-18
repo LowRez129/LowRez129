5d2b9955ee91b77be6619e62cf88b05951d8fed4
LowRez129 <114649755+LowRez129@users.noreply.github.com>
2024-03-18 19:38:04 +1300

    Project section's image now has buttons beside it, to send the user to the github repo or live website.

--- src/components/projects/Project.tsx
+++ src/components/projects/Project.tsx
@@ -1,9 +1,18 @@
-import { ProjectContainer, ProjectDescription, ProjectImage, ProjectName } from "./ProjectStyles";
+import { LinkButton, LinkButtonContainer, ProjectContainer, ProjectDescription, ProjectImage, ProjectName } from "./ProjectStyles";
 
 export default function Project ({ src, name, description } : { src: string, name: string, description: string }) {
+    const neolexicon_website = "https://neolexicon-client.onrender.com";
+    const neolexicon_github = "https://github.com/LowRez129/neolexicon";
+    const goto_neolexicon_website = () => window.location.href = neolexicon_website;
+    const goto_neolexicon_github = () => window.location.href = neolexicon_github;
+
     return (
         <ProjectContainer>
             <ProjectImage src={src}/>
+            <LinkButtonContainer>
+                <LinkButton onClick={goto_neolexicon_website}>Neolexicon</LinkButton>
+                <LinkButton onClick={goto_neolexicon_github}>Github Repo</LinkButton>
+            </LinkButtonContainer>
             <ProjectName>{name}</ProjectName>
             <ProjectDescription>{description}</ProjectDescription>
         </ProjectContainer>
--- src/components/projects/ProjectStyles.tsx
+++ src/components/projects/ProjectStyles.tsx
@@ -3,13 +3,37 @@ import styled from "styled-components";
 const ProjectContainer = styled.div`
     display: grid;
     background-color: aquamarine;
-    grid-template-columns: 100vw;
+    grid-template-columns: 60% 1fr;
     grid-template-rows: 1fr 10% 1fr;
+    grid-template-areas: 
+        "project_image button_container"
+        "project_name project_name"
+        "project_description project_description"
+    ;
 `;
 
 const ProjectImage = styled.img`
-    height: 100%;
-    width: 100%;
+    height: 200px;
+    width: 200px;
+    padding: 10px;
+    justify-self: center;
+    align-self: center;
+    grid-area: project_image;
+`;
+
+const LinkButtonContainer = styled.div`
+    display: grid;
+    grid-area: button_container;
+    justify-self: center;
+    align-self: center;
+    gap: 10px;
+`;
+
+const LinkButton = styled.button`
+    background-color: green;
+    &:active {
+        background-color: yellowgreen;
+    }
 `;
 
 const ProjectName = styled.div`
@@ -17,12 +41,17 @@ const ProjectName = styled.div`
     display: grid;
     justify-content: center;
     align-items: center;
+    grid-area: project_name;
 `;
 
 const ProjectDescription = styled.div`
     background-color: deepskyblue;
     padding: 10px;
     overflow-wrap: break-word;
+    grid-area: project_description;
 `;
 
-export { ProjectContainer, ProjectImage, ProjectName, ProjectDescription };
+export { 
+    ProjectContainer, ProjectImage, LinkButtonContainer, 
+    LinkButton, ProjectName, ProjectDescription 
+};
